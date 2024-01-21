1c0cd89d88b7831a773a9ab771a76e7e87f4580d
ankitjhagithub21 <ajha45075@gmail.com>
2024-01-21 11:38:33 +0530

    fix white screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.js';
+
 import "./App.css"
 import Home from './components/Home';
 import Navbar from './components/Navbar';
